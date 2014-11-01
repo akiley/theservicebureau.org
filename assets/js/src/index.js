ce1f6339fbb935844281a01f7aeec88d7e0660af
rubillionaire <mail@rubenrodriguez.me>
2014-11-01 19:45:06 -0400

    Random placement
    This may not be the perferred solution, because it requires a predefined post area to randomly place images within, but its the only way to do it without knowing the size of the images involved before hand. We can only find out image height and width when the image is first loaded. Saving a reference to these locations could work, but then none of them would be laid out until all of them were loaded. In this approach, as they load, they get placed randomly. If the random positions happen to overlap, a new random position is assigned, and no exact overlapping is ensured.

--- assets/js/src/index.js
+++ assets/js/src/index.js
@@ -13,51 +13,154 @@ $(document).ready(function() {
 		});
     };
 
-    // This is the timer that will be
-    // used to have the menus fade out.
-    // This can be cleared by some other
-    // input. See below.
-    var fadeOutTimeout;
+    // Layout image piles.
+    layoutImagePiles();
 
-    // Menus animate out on start.
-    fadeMenuOut();
+	// Setup the menu interaction.
+    setupMenuInteraction();
 
-    // Menus fade in when scrolling.
-    $(window).scroll(function() {
-		fadeMenuIn();
-    });
+    function layoutImagePiles () {
+        var piles = {};
+        var randomOffset = function () {
+            var maxRandomOffset = 30;
+            var offsetInterval = 10;
+            var r = Math.random() * maxRandomOffset;
+            var i = Math.floor(r/offsetInterval);
+            if (Math.random() > 0.3) {
+                Math.ceil(r/10);
+            }
+            if (Math.random() > 0.5) {
+                i *= -1;
+            }
+            return i * offsetInterval;
+        };
+        /*
+        Need to know the height and
+        width of each image to determine
+        its random offset. To do so, we
+        need a reference to the original
+        loaded image. */
+        $("img").load(function () {
+            var img = $(this);
+            var parent = img.parent();
+            var grandParent = parent.parent();
 
-    // Menus fade out when you stop scrolling.
-	$(window).scrollStopped(function(){
+            /* 
+            We are only interested in randomly
+            offsetting images that are in a .images
+            parent, and .post grandparent. */
+            if (grandParent.hasClass('post')) {
+                var pileMaxHeight = parent.height();
+                var pileMaxWidth = parent.width();
+
+                /*
+                Make an id for the grandparent
+                based on the classes it has.
+                Each post should be a different
+                project, with a different name.
+                So these should be unique. */
+                var pileId = grandParent.attr('class')
+                    .replace(/ /g, '--');
+
+                /*
+                If this is the first image in
+                a pile, make an array to store
+                a reference to all of the images
+                in the pile, to determine their
+                offsets. */
+                if (!(pileId in piles)) {
+                    piles[pileId] = [];
+                    first = true;
+                }
+
+                var height = img.height();
+                var width = img.width();
+
+                var placement = placeImage();
+
+                function placeImage () {
+                    return {
+                        left: ((pileMaxWidth - width)/2) +
+                              randomOffset(),
+                        top: ((pileMaxHeight - height)/2) +
+                              randomOffset()
+                    };
+                }
+
+                /*
+                Ensure images are not cover
+                the same area in a pile. */
+                ensureNoOverlap();
+                console.log(img);
+                function ensureNoOverlap () {
+                    console.log('ensureNoOverlap');
+                    piles[pileId].forEach(function (i) {
+                        if ((placement.top === i.top) |
+                            (placement.left === i.left)) {
+                            placement = placeImage();
+                            ensureNoOverlap();
+                        }
+                    });
+                }
+
+                img.css('top', placement.top);
+                img.css('left', placement.left);
+
+                var i = placement;
+                i.img = img;
+
+                piles[pileId].push(i);
+            }
+        });
+    }
+
+    function setupMenuInteraction () {
+		// This is the timer that will be
+		// used to have the menus fade out.
+		// This can be cleared by some other
+		// input. See below.
+		var fadeOutTimeout;
+
+		// Menus animate out on start.
 		fadeMenuOut();
-	});
 
-	// Hovering over a menu clears the
-	// interval that is being waited on
-	// for the fadeMenuOut to happen.
-	$(".menu-button").mouseenter(function () {
-		$(".menu-button").stop();
-		if (fadeOutTimeout) {
-			clearInterval(fadeOutTimeout);
+		// Menus fade in when scrolling.
+		$(window).scroll(function() {
+			fadeMenuIn();
+		});
+
+		// Menus fade out when you stop scrolling.
+		$(window).scrollStopped(function(){
+			fadeMenuOut();
+		});
+
+		// Hovering over a menu clears the
+		// interval that is being waited on
+		// for the fadeMenuOut to happen.
+		$(".menu-button").mouseenter(function () {
+			$(".menu-button").stop();
+			if (fadeOutTimeout) {
+				clearInterval(fadeOutTimeout);
+			}
+		});
+
+		// Unhovering a menu reinitializes the
+		// fade out timer.
+		$(".menu-button").mouseleave(function () {
+			fadeMenuOut();
+		});
+
+		function fadeMenuOut () {
+			$(".menu-button").stop();
+			fadeOutTimeout = setTimeout(function () {
+				$(".menu-button").fadeOut(500);
+			}, 2000);
 		}
-	});
 
-	// Unhovering a menu reinitializes the
-	// fade out timer.
-	$(".menu-button").mouseleave(function () {
-		fadeMenuOut();
-	});
-
-	function fadeMenuOut () {
-		$(".menu-button").stop();
-		fadeOutTimeout = setTimeout(function () {
-			$(".menu-button").fadeOut(500);
-		}, 2000);
-	}
-
-	function fadeMenuIn () {
-		$(".menu-button").stop().fadeIn(50);
-	}
+		function fadeMenuIn () {
+			$(".menu-button").stop().fadeIn(50);
+		}
+    }
 });
 
 
