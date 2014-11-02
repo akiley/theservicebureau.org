5d75952e0b7c325c1a6bfbf2b23be9bc544b0e32
akiley <antherkiley@gmail.com>
2014-11-02 09:37:53 -0800

    Messed up sync
    positioning messed up and jQuery not working

--- assets/js/dist/src.js
+++ assets/js/dist/src.js
@@ -3,7 +3,16 @@
 
 
 $(document).ready(function() {
+
+
+	$("#store-button").hover(function(){
+		$(".menu-button").stop().css('display','block');
+	});
+
+
+	// to make buttons appear and persist for 2000 milliseconds on scroll
 	
+<<<<<<< Updated upstream
 	$.fn.scrollStopped = function(callback) {
 		$(this).scroll(function(){
 			var self = this, $this = $(self);
@@ -161,6 +170,54 @@ $(document).ready(function() {
 			$(".menu-button").stop().fadeIn(50);
 		}
     }
+=======
+	$.fn.scrollStopped = function(callback) {           
+	        $(this).scroll(function(){
+	            var self = this, $this = $(self);
+	            if ($this.data('scrollTimeout')) {
+	              clearTimeout($this.data('scrollTimeout'));
+	            }
+	            $this.data('scrollTimeout', setTimeout(callback,250,self));
+	        });
+	    };
+
+	$(function () {
+		$(".menu-button").delay(2000).fadeOut(500);
+	});
+
+	$(window).scrollStopped(function(){
+	    $(".menu-button").stop().delay(2000).fadeOut(500);
+	});                   
+
+	$(window).scroll(function() {
+		$(".menu-button").stop().fadeIn(50);
+	});
+
+
+
+
+
+
+
+
+	// to make buttons grow
+
+	// $("#store-button").hover(function(){
+ //  		$(this).animate({height:"150", width:"150", marginLeft:"-25", marginTop:"-75"}, 50);
+ //  		},
+	// 	function(){
+ //  		$(this).animate({height:"100", width:"100", marginLeft:"0", marginTop:"-50"}, 50);
+	// });
+
+
+
+	
+
+
+
+
+
+>>>>>>> Stashed changes
 });
 
 
